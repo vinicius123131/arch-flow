7d3f9889a1e812b68ff92af93ba5452c6774fc4e
vinicius123131 <vini989073599@gmail.com>
2024-02-19 16:52:26 -0300

    feat: criação da implementação do ArchFlow com funções nativas

--- core/entities/ArchFlow.py
+++ core/entities/ArchFlow.py
@@ -1,4 +1,19 @@
-class ArchFlow:
+from abc import ABC, abstractmethod
+from core.entities.DirectoryCreator import DirectoryCreator
+from core.entities.DirectoryExplorer import DirectoryExplorer
+from core.entities.StringManipulator import StringManipulator
 
-    def __init__(self, project):
-        self.project = project
+
+class ArchFlow(ABC):
+    def __init__(self):
+        self.DirectoryCreator = DirectoryCreator()
+        self.DirectoryExplorer = DirectoryExplorer()
+        self.StringManipulator = StringManipulator()
+
+    @abstractmethod
+    def create_project(self):
+        pass
+
+    @abstractmethod
+    def functions_flow(self):
+        pass
